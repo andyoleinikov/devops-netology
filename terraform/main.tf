12b75ce3fd4be2b9336c5cdf6456353fe0584ded
Andrey Oleinikov <dyt321@ya.ru>
2021-11-11 21:51:03 +0300

    added part 7.3

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+locals {
+  web_instance_count_map = {
+  stage = 1
+  prod = 2
+  }
+}
+
+locals {
+  web_instance_type_map = {
+  stage = "t2.micro"
+  prod = "t3.large"
+  }
+}
+
+
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -26,6 +42,23 @@ resource "aws_instance" "web" {
     Name = "Netology"
   }
 } 
+
+
+resource "aws_instance" "count_web" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  count = local.web_instance_count_map[terraform.workspace]
+}
+
+resource "aws_instance" "for_each_web" {
+  for_each = local.web_instance_type_map
+  ami = data.aws_ami.ubuntu.id
+  instance_type = each.value
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
